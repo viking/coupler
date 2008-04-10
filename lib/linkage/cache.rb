f7448ef4dabcf3731629b8bc355dc762150ea2ac
Jeremy Stephens <viking415@gmail.com>
2008-04-10 17:14:24 +0000

    Reimplemented the cache in hopes of a increase in performance.

--- lib/linkage/cache.rb
+++ lib/linkage/cache.rb
@@ -6,23 +6,54 @@ module Linkage
     def initialize(resource_name)
       @resource  = Linkage::Resource.find(resource_name)
       @cache     = Hash.new 
+      @rev_cache = Hash.new
       @misses    = 0
       @fetches   = 0
+      @reclaim   = lambda do |value_id|
+        # delete key from @cache when value gets GC'd
+        if @rev_cache.has_key?(value_id)
+          key = @rev_cache[value_id]
+          @cache[key] = :gone
+          @rev_cache.delete(value_id)
+        end
+      end
     end
 
     def add(key, record)
-      @cache[key] = WeakRef.new(record)
+      id = record.object_id 
+      @cache[key]    = id
+      @rev_cache[id] = key
+      ObjectSpace.define_finalizer(record, @reclaim)
       record
     end
 
     def fetch(key)
       @fetches += 1
-      record = @cache[key]
-      unless record.weakref_alive?
-        # re-fetch the row
-        @misses += 1
-        record = add(key, @resource.select_one(key))
+      begin
+        old_critical = Thread.critical
+        Thread.critical = true
+
+        id = @cache[key]
+        case id
+        when :gone
+          record = recover(key)
+        when nil
+          record = nil
+        else
+          begin
+            record = ObjectSpace._id2ref(id)
+          rescue RangeError
+            puts "uncaught"
+            record = recover(key)
+          end
+        end
+      rescue Exception => boom
+        debugger
+        puts "error!"
+      ensure
+        Thread.critical = old_critical
       end
+
       record
     end
 
@@ -33,5 +64,11 @@ module Linkage
     def keys
       @cache.keys
     end
+
+    private
+      def recover(key)
+        @misses += 1
+        add(key, @resource.select_one(key))
+      end
   end
 end
